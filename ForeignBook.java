2703f18d2207516436d32e31186e20ab104cbbdf
Valeria Shevchenko <valeriya-shevchenko-2002@mail.ru>
2021-12-12 13:04:40 +0700

    Конструкторы, применение protected

--- ForeignBook.java
+++ ForeignBook.java
@@ -11,4 +11,16 @@ class ForeignBook extends Book
     {
         this.language = language;
     }
+
+    //Конструктор с параметром
+    public ForeignBook(String title, Author author, int pages_number, String genre, Publishing publishing, int publication_year, String language) : Book(title, author, pages_number, genre, publishing, publication_year)
+    {
+        this->language = language;
+    }
+
+    //Конструктор с параметром
+    public ForeignBook(String title, Author author, int pages_number, String genre, Reader reader, Publishing publishing, int publication_year, String language) : Book(title, author, pages_number, genre, reader, publishing, publication_year)
+    {
+        this->language = language;
+    }
 }
